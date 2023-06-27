1401ae566ba5ecca985f43fc3c1dda419337c89b
muzzamilr <mmuzzamil90@gmail.com>
2023-06-27 21:52:50 +0500

    complete part1 unicafe till 1.10 exercise

--- part1/unicafe/src/Statistics.jsx
+++ part1/unicafe/src/Statistics.jsx
@@ -1,14 +1,22 @@
+const StatisticLine = ({ text, value }) => {
+  return (
+    <p>
+      {text} {value}
+    </p>
+  );
+};
+
 export const Statistics = ({ good, neutral, bad, total, avg, positive }) => {
   return (
     <>
       {good || bad || neutral ? (
         <div>
-          <p>good {good}</p>
-          <p>neutral {neutral}</p>
-          <p>bad {bad}</p>
-          <p>all {total}</p>
-          <p>average {avg}</p>
-          <p>positive {positive}%</p>
+          <StatisticLine text="good" value={good} />
+          <StatisticLine text="neutral" value={neutral} />
+          <StatisticLine text="bad" value={bad} />
+          <StatisticLine text="all" value={total} />
+          <StatisticLine text="avg" value={avg} />
+          <StatisticLine text="positive" value={positive + " %"} />
         </div>
       ) : (
         <p>No feedback given</p>
