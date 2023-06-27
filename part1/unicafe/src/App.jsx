6954fa04166fdfacf291d3a98953bb8978852050
muzzamilr <mmuzzamil90@gmail.com>
2023-06-27 21:40:15 +0500

    complete part1 unicafe till 1.7 exercise

---
+++ part1/unicafe/src/App.jsx
@@ -0,0 +1,55 @@
+import { useState } from "react";
+
+const App = () => {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+  const [total, setTotal] = useState(0);
+  const [avg, setAvg] = useState(0);
+  const [positive, setPositive] = useState(0);
+
+  const handleGood = () => {
+    setGood((good) => good + 1);
+    setTotal((total) => total + 1);
+    if (total) {
+      setAvg((good - bad) / total);
+      setPositive((good * 100) / total);
+    }
+  };
+
+  const handleNeutral = () => {
+    setNeutral((neutral) => neutral + 1);
+    setTotal((total) => total + 1);
+    if (total) {
+      setAvg((good - bad) / total);
+      setPositive((good * 100) / total);
+    }
+  };
+
+  const handleBad = () => {
+    setBad((bad) => bad + 1);
+    setTotal((total) => total + 1);
+    if (total) {
+      setAvg((good - bad) / total);
+      setPositive((good * 100) / total);
+    }
+  };
+
+  return (
+    <div>
+      <h1>give feedback</h1>
+      <button onClick={handleGood}>good</button>
+      <button onClick={handleNeutral}>neutral</button>
+      <button onClick={handleBad}>bad</button>
+      <h2>statistics</h2>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
+      <p>all {total}</p>
+      <p>average {avg}</p>
+      <p>positive {positive}%</p>
+    </div>
+  );
+};
+
+export default App;
