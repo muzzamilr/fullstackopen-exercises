8b7c83eb71e5d181145b96a1e6599253cf03dc5a
muzzamilr <mmuzzamil90@gmail.com>
2023-06-30 15:45:37 +0500

    complete part2 till 2.9 exercise

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -2,18 +2,25 @@ import { useState } from "react";
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "111-111-111" },
+    { name: "Arto Hellas", number: "040-123456", id: 1 },
+    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
+    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
+    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
   ]);
+  const [filteredPersons, setFilteredPersons] = useState([]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
+  const [showAll, setShowALl] = useState(true);
 
   const handleFormSubmit = (e) => {
     e.preventDefault();
+    if (newName === "" || newNumber === "") return;
     if (persons.filter((obj) => obj.name === newName).length > 0) {
       alert(`${newName} is already added to phonebook`);
       return;
     }
     setPersons((per) => [...per, { name: newName, number: newNumber }]);
+    console.log(filteredPersons);
   };
 
   const handleNameChange = (e) => {
@@ -24,9 +31,24 @@ const App = () => {
     setNewNumber(e.target.value);
   };
 
+  const handleFilterChange = (e) => {
+    setShowALl(false);
+    setFilteredPersons(
+      persons.filter((obj) =>
+        obj.name.toLowerCase().includes(e.target.value.toLowerCase())
+      )
+    );
+
+    if (e.target.value === "") setShowALl(true);
+  };
+
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        filter shown with <input onChange={handleFilterChange} />
+      </div>
+      <h2>add a new</h2>
       <form onSubmit={handleFormSubmit}>
         <div>
           name: <input onChange={handleNameChange} value={newName} />
@@ -39,9 +61,13 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map((obj) => (
-        <p key={obj.name}>{obj.name + " " + obj.number} </p>
-      ))}
+      {showAll
+        ? persons.map((obj) => (
+            <p key={obj.name}>{obj.name + " " + obj.number} </p>
+          ))
+        : filteredPersons.map((obj) => (
+            <p key={obj.name}>{obj.name + " " + obj.number} </p>
+          ))}
     </div>
   );
 };
