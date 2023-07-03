e28e6a257bc4435671d94c50280af553689cdf7a
muzzamilr <mmuzzamil90@gmail.com>
2023-07-03 19:25:29 +0500

    complete part3 till 3.22 exercise

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -17,41 +17,12 @@ app.use(
 );
 app.use(express.static("dist"));
 
-let persons = [
-  {
-    id: 1,
-    name: "Arto Hellas",
-    number: "040-123456",
-  },
-  {
-    id: 2,
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
-  },
-  {
-    id: 3,
-    name: "Dan Abramov",
-    number: "12-43-234345",
-  },
-  {
-    id: 4,
-    name: "Mary Poppendieck",
-    number: "39-23-6423122",
-  },
-];
-
-const generateId = () => {
-  const maxId = persons.length > 0 ? Math.max(...persons.map((n) => n.id)) : 0;
-  return maxId + 1;
-};
-
 app.get("/", (request, response) => {
   response.send("<h1>Hello World!</h1>");
 });
 
 app.get("/info", (request, response) => {
   const date = new Date();
-  let personsLen;
   Person.find({}).then((persons) =>
     response.send(
       `<div><p>Phonebook has info for ${persons.length} people</p><p>${date}</p></div>`
@@ -63,7 +34,7 @@ app.get("/api/persons", (request, response) => {
   Person.find({}).then((persons) => response.json(persons));
 });
 
-app.get("/api/persons/:id", (request, response) => {
+app.get("/api/persons/:id", (request, response, next) => {
   Person.findById(request.params.id)
     .then((person) => {
       if (person) {
@@ -84,12 +55,6 @@ app.post("/api/persons", (request, response, next) => {
       error: "content missing",
     });
   }
-
-  // if (persons.find((x) => x.name.toLowerCase() === body.name.toLowerCase())) {
-  //   return response.status(400).json({
-  //     error: "name must be unique",
-  //   });
-  // }
 
   const person = new Person({
     name: body.name,
