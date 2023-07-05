5b15ec6801a5516149193ffaac04c8621701f324
muzzamilr <mmuzzamil90@gmail.com>
2023-07-05 21:09:41 +0500

    change request logger to morgan

--- part4/blog-list/utils/middleware.js
+++ part4/blog-list/utils/middleware.js
@@ -1,12 +1,13 @@
 const logger = require("./logger");
+const morgan = require("morgan");
 
-const requestLogger = (request, response, next) => {
-  logger.info("Method:", request.method);
-  logger.info("Path:  ", request.path);
-  logger.info("Body:  ", request.body);
-  logger.info("---");
-  next();
-};
+morgan.token("data", (request) => {
+  return request.method === "POST" ? JSON.stringify(request.body) : " ";
+});
+
+const requestLogger = morgan(
+  ":method :url :status :res[content-length] - :response-time ms :data"
+);
 
 const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: "unknown endpoint" });
