a5564a2c975c18c40846c903b2c1c80e45dbf32d
muzzamilr <mmuzzamil90@gmail.com>
2023-07-05 21:07:20 +0500

    refactor app

--- part4/blog-list/models/blog.js
+++ part4/blog-list/models/blog.js
@@ -7,9 +7,6 @@ const blogSchema = new mongoose.Schema({
   likes: Number,
 });
 
-const mongoUrl = process.env.MONGODB_URI;
-mongoose.connect(mongoUrl);
-
 blogSchema.set("toJSON", {
   transform: (document, returnedObject) => {
     returnedObject.id = returnedObject._id.toString();
