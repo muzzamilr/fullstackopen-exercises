76487744983e80937c4fb33fcc84f25828451954
muzzamilr <mmuzzamil90@gmail.com>
2023-07-05 22:37:33 +0500

    complete part4 till 4.10 exercise

--- part4/blog-list/tests/blog_api.test.js
+++ part4/blog-list/tests/blog_api.test.js
@@ -1,8 +1,15 @@
 const mongoose = require("mongoose");
 const supertest = require("supertest");
 const app = require("../app");
+const Blog = require("../models/blog");
+const blogs = require("./blog_lists");
 
 const api = supertest(app);
+
+beforeEach(async () => {
+  await Blog.deleteMany({});
+  await Blog.insertMany(blogs.oneBlog);
+});
 
 test("length of blogs", async () => {
   const res = await api.get("/api/blogs");
@@ -13,6 +20,20 @@ test("id property of blog", async () => {
   const res = await api.get("/api/blogs");
   const allIds = res.body.map((blog) => blog.id);
   allIds.forEach((id) => expect(id).toBeDefined());
+});
+
+test("blog is added", async () => {
+  const blog = {
+    title: "blog",
+    author: "author",
+    url: "https://www.fullstackopen.com",
+    likes: 1,
+  };
+  await api
+    .post("/api/blogs")
+    .send(blog)
+    .expect(201)
+    .expect("Content-Type", /application\/json/);
 });
 
 afterAll(async () => {
