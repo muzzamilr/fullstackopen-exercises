157630b2f1946391114551c6a872bb4e0ec5e532
muzzamilr <mmuzzamil90@gmail.com>
2023-07-05 22:26:25 +0500

    complete part4 till 4.9 exercise

--- part4/blog-list/tests/blog_api.test.js
+++ part4/blog-list/tests/blog_api.test.js
@@ -4,9 +4,15 @@ const app = require("../app");
 
 const api = supertest(app);
 
-test("blogs are returned as json", async () => {
+test("length of blogs", async () => {
   const res = await api.get("/api/blogs");
   expect(res.body).toHaveLength(1);
+});
+
+test("id property of blog", async () => {
+  const res = await api.get("/api/blogs");
+  const allIds = res.body.map((blog) => blog.id);
+  allIds.forEach((id) => expect(id).toBeDefined());
 });
 
 afterAll(async () => {
