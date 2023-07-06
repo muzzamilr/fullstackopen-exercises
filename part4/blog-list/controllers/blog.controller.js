67ad485ec73573e06ca80bac8e275e18da6e9956
muzzamilr <mmuzzamil90@gmail.com>
2023-07-06 21:28:26 +0500

    complete part4 till 4.22 exercise

--- part4/blog-list/controllers/blog.controller.js
+++ part4/blog-list/controllers/blog.controller.js
@@ -3,26 +3,13 @@ const blogsRouter = require("express").Router();
 const Blog = require("../models/blog");
 const User = require("../models/user");
 
-const getTokenFrom = (request) => {
-  const authorization = request.get("authorization");
-  if (authorization && authorization.startsWith("Bearer ")) {
-    return authorization.replace("Bearer ", "");
-  }
-  return null;
-};
-
 blogsRouter.get("/", async (request, response) => {
   const blogs = await Blog.find({}).populate("user", { username: 1, name: 1 });
   response.json(blogs);
 });
 
 blogsRouter.post("/", async (request, response) => {
-  const { body } = request;
-  const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET);
-  if (!decodedToken.id) {
-    return response.status(401).json({ error: "token invalid" });
-  }
-  const user = await User.findById(decodedToken.id);
+  const { body, user } = request;
 
   const blog = new Blog({
     title: body.title,
@@ -37,7 +24,15 @@ blogsRouter.post("/", async (request, response) => {
 });
 
 blogsRouter.delete("/:id", async (request, response) => {
-  await Blog.deleteOne({ _id: request.params.id });
+  const { id } = request.params;
+  const user = request.user;
+  const blog = await Blog.findById({ _id: id });
+
+  if (blog && blog.user.toString() !== user.id.toString()) {
+    response.status(401).json({ error: "Unauthorized" });
+  }
+
+  await Blog.deleteOne({ _id: id });
   response.status(204).end();
 });
 
--- part4/blog-list/utils/middleware.js
+++ part4/blog-list/utils/middleware.js
@@ -1,5 +1,7 @@
 const logger = require("./logger");
 const morgan = require("morgan");
+const User = require("../models/user");
+const jwt = require("jsonwebtoken");
 
 morgan.token("data", (request) => {
   return request.method === "POST" ? JSON.stringify(request.body) : " ";
@@ -13,6 +15,26 @@ const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: "unknown endpoint" });
 };
 
+const tokenExtractor = (request, response, next) => {
+  const authorization = request.get("authorization");
+  if (authorization && authorization.toLowerCase().startsWith("bearer ")) {
+    request.token = authorization.substring(7);
+  }
+
+  next();
+};
+
+const userExtractor = async (request, response, next) => {
+  const token = request.token;
+  if (token) {
+    const decodedToken = jwt.verify(token, process.env.SECRET);
+    const user = await User.findById(decodedToken.id);
+    request.user = user;
+  }
+
+  next();
+};
+
 const errorHandler = (error, request, response, next) => {
   logger.error(error.message);
 
@@ -20,8 +42,13 @@ const errorHandler = (error, request, response, next) => {
     return response.status(400).send({ error: "malformatted id" });
   } else if (error.name === "ValidationError") {
     return response.status(400).json({ error: error.message });
+  } else if (error.name === "JsonWebTokenError") {
+    return response.status(400).json({ error: error.message });
+  } else if (error.name === "TokenExpiredError") {
+    return response.status(401).json({
+      error: "token expired",
+    });
   }
-
   next(error);
 };
 
@@ -29,4 +56,6 @@ module.exports = {
   requestLogger,
   unknownEndpoint,
   errorHandler,
+  tokenExtractor,
+  userExtractor,
 };
